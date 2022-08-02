47332f0673047079f0e81ba6c057a208825f1f1c
neriiavr <avrupalineriman@gmail.com>
2022-08-02 21:36:36 +0200

    dynamic currency convertion implementation

--- currency/currency.js
+++ currency/currency.js
@@ -1,18 +1,41 @@
+var myHeaders = new Headers();
+myHeaders.append("apikey", "XJHIxfvMkz2q6OzJlM9bfuGCWGtEtQUJ");
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+};  
+
 function eToDolar () {
     const inputsValue = document.getElementById("eToDolar").value
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "XJHIxfvMkz2q6OzJlM9bfuGCWGtEtQUJ");
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-        headers: myHeaders
-    };  
-    
     fetch(`https://api.apilayer.com/currency_data/convert?to=USD&from=EUR&amount=${inputsValue}`, requestOptions)
     .then(response => response.json())
     .then(function (responseObject) {
-        document.getElementById("dolarConverted").innerText = responseObject.result
+        document.getElementById("eToDolarConverted").innerText = responseObject.result
     })
 }
 
+function dToEuro () {
+    const inputsValue = document.getElementById("dToEuro").value
+    fetch(`https://api.apilayer.com/currency_data/convert?to=EUR&from=USD&amount=${inputsValue}`, requestOptions)
+    .then(response => response.json())
+    .then(function (responseObject) {
+        document.getElementById("dToEuroConverted").innerText = responseObject.result
+    })
+}
+
+function convert() {
+    const inputsValue = document.getElementById("input").value
+    var select= document.getElementById("from")
+    const valueFrom = from.options[select.selectedIndex].value
+    var selectTo= document.getElementById("to")
+    const valueTo = to.options[selectTo.selectedIndex].value
+
+    fetch(`https://api.apilayer.com/currency_data/convert?to=${valueTo}&from=${valueFrom}&amount=${inputsValue}`, requestOptions)
+    .then(response => response.json())
+    .then(function (responseObject) {
+        document.getElementById("selectedConverted").innerText = responseObject.result
+    })
+}
