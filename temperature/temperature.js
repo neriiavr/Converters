c7b9710af319c4043ef44912076ab52280558ae5
neriiavr <avrupalineriman@gmail.com>
2023-01-07 17:43:33 +0100

    added temperature

--- temperature/temperature.js
+++ temperature/temperature.js
@@ -7,3 +7,24 @@ function celToFahFormula() {
 
     document.getElementById("celToFahDone").innerText = celToFahConver
 }
+
+function fahToCelFormula() {
+    const inputsValue= document.getElementById("fahToCel").value
+    var fahToCelConver = ""
+    fahToCelConver = (inputsValue - 32) * 5/9
+
+    console.log(inputsValue)
+
+    document.getElementById("fahToCelDone").innerText = fahToCelConver
+}
+
+function ranToCelFormula() {
+    const inputsValue= document.getElementById("ranToCel").value
+    var ranToCelConver = ""
+    ranToCelConver = inputsValue * 5/9 - 273.15 
+
+    console.log(inputsValue)
+
+    document.getElementById("ranToCelDone").innerText = ranToCelConver
+}
+
